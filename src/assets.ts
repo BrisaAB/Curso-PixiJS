a769a83c652d9f62a53ab406c9aa6407f2c0aa9a
BrisaAB <brisantuna@gmail.com>
2023-07-22 12:19:35 -0300

    agrego imagenes para tarea clase 3

---
+++ src/assets.ts
@@ -0,0 +1,12 @@
+export const assets = [
+    {url:"./terry.png", name:"normalTerry"},
+    {url:"./caña.png", name:"normalcania"},
+    {url:"./cielo.jpg", name:"normalCielo"},
+    {url:"./fondoAgua.jpg", name:"normalAgua"},
+    {url:"./fondoTierraEditado.png", name:"normalTierra"},
+    {url:"./Pez.png", name:"normalPez"},
+    {url:"./botonSF.png", name:"normalBoton"},
+    {url:"./estrellaLlenaSF.png", name:"normalEstrellaL"},
+    {url:"./estrellaVaciaSF.png", name:"normalestrellaV"},
+    {url:"./tableroSF.png", name:"normalTablero"}
+];
