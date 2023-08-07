6d542d5754eac37d7eb0eafca91554f0c439774a
BrisaAB <brisantuna@gmail.com>
2023-08-07 20:40:50 -0300

    inicialicé keyboard en index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Application, Loader} from 'pixi.js'
 import { assets } from './assets';
 import { Scene } from './Scenes/Scene';
 import { Cartel } from './Scenes/Cartel';
+import { Keyboard } from './utils/keyboard';
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -11,6 +12,8 @@ const app = new Application({
 	width: 1280,
 	height: 720
 });
+
+Keyboard.initialize();
 window.addEventListener("resize",()=>{
 		console.log("resized");
 		//-------------TAMANO DEL JUEGO-------------------
